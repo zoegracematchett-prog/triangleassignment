0e88531575f162cd593f30db83710d8781ed0ef1
Zoe Matchett <zoegracematchett@gmail.com>
2025-11-08 13:32:37 +0000

    fix: renamed number to num

--- printNumberOfAnswers.m
+++ printNumberOfAnswers.m
@@ -1,6 +1,6 @@
 function printNumberOfAnswers(num)
     for i = 1:num
         istriangle([i, i+1, i+2])
-        fprintf('This is number: %d\n', i);
+        fprintf('This is num: %d\n', i);
     end
    
